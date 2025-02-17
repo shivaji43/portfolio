0e921615e2085fe760a76b01003dfda564c34074
shivaji43 <shivajijraut@gmail.com>
2025-02-18 02:18:01 +0530

    added projects and experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
-});
+  weight: ["400"],
+})
 
 export const metadata: Metadata = {
   title: "Shivaji Raut",
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${spaceGrotesk.className} ${spaceGrotesk.className} antialiased`}
       >
         {children}
       </body>
